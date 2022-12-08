0ada3d8f8ca5a3558fe59f97e2f043de32d8379a
丘友垚 <wx_117c4d9809404e4e9556cde6e7e150b6@git.code.tencent.com>
2022-12-08 23:06:25 +0800

    添加路由匹配

--- gee/router.go
+++ gee/router.go
@@ -3,21 +3,30 @@ package gee
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 type router struct {
+	roots    map[string]*node
 	handlers map[string]HandlerFunc
 }
 
 func newRouter() *router {
 	return &router{
+		roots:    make(map[string]*node),
 		handlers: make(map[string]HandlerFunc),
 	}
 }
 
 func (router *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	parts := parsePattern(pattern)
 	log.Printf("Route %4s - %s ", method, pattern)
 	routeKey := buildRouteKey(method, pattern)
+	_, ok := router.roots[method]
+	if !ok {
+		router.roots[method] = &node{}
+	}
+	router.roots[method].insert(pattern, parts, 0)
 	router.handlers[routeKey] = handler
 }
 
@@ -31,6 +40,20 @@ func (router *router) handle(context *Context) {
 			return
 		}
 	}
+}
+
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
 }
 
 func buildRouteKey(method string, pattern string) string {
